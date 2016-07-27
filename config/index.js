a76eabe66e50b4655b5435b73fc094be73dc6384
maifei <maifei@in66.com>
2016-07-28 01:04:09 +0800

    add config

---
+++ config/index.js
@@ -0,0 +1,12 @@
+var path = require('path')
+
+module.exports = {
+  build: {
+    env: require('./prod.env')
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    proxyTable: {}
+  }
+}
--- devServer.js
+++ devServer.js
@@ -1,9 +1,14 @@
+'use strict'
+
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
-const config = require('./webpack.config')
+const webpackConfig = require('./webpack.config')
+const config = require('./config')
 
-const server = new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
+let port = process.env.PORT || config.dev.port
+
+const server = new WebpackDevServer(webpack(webpackConfig), {
+  publicPath: webpackConfig.output.publicPath,
   hot: true,
   historyApiFallback: true,
   stats: {
@@ -11,10 +16,10 @@ const server = new WebpackDevServer(webpack(config), {
   }
 })
 
-server.listen(3000, 'localhost', function (err) {
+server.listen(port, 'localhost', function (err) {
   if (err) {
     return console.log(err);
   }
 })
 
-console.log('Listening at http://localhost:3000/')
+console.log('Listening at http://localhost:' + port + '\n')
